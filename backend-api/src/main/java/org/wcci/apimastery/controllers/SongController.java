03c562c75133a2780d77f94095e503fe245132cd
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-13 15:10:04 -0400

    working on adding comment feature

--- backend-api/src/main/java/org/wcci/apimastery/controllers/SongController.java
+++ backend-api/src/main/java/org/wcci/apimastery/controllers/SongController.java
@@ -1,9 +1,12 @@
 package org.wcci.apimastery.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.wcci.apimastery.entities.Album;
 import org.wcci.apimastery.entities.Artist;
+import org.wcci.apimastery.entities.Comment;
 import org.wcci.apimastery.entities.Song;
 import org.wcci.apimastery.repositories.SongRepository;
+import org.wcci.apimastery.storage.CommentStorage;
 import org.wcci.apimastery.storage.SongStorage;
 
 import java.util.Collection;
@@ -11,9 +14,11 @@ import java.util.Collection;
 @RestController
 public class SongController {
     SongStorage songStorage;
+    CommentStorage commentStorage;
 
-    public SongController(SongStorage songStorage) {
+    public SongController(SongStorage songStorage, CommentStorage commentStorage) {
         this.songStorage = songStorage;
+        this.commentStorage = commentStorage;
     }
 
     @GetMapping("/api/songs/")
@@ -30,9 +35,27 @@ public class SongController {
     public Song addSong(@RequestBody Song song){
         return songStorage.save(song);
     }
+
     @DeleteMapping("/api/songs/delete/")
     public Collection<Song> deleteSong(@PathVariable long id) {
         songStorage.deleteSongById(id);
         return songStorage.retrieveAllSongs();
     }
+
+    @PostMapping("/api/comments/add/")
+    public Comment addComment(@RequestBody long songId,String authorName, String commentText){
+        Song song = songStorage.retrieveSongById(songId);
+        Comment commentToAdd = new Comment(authorName, commentText, song);
+        commentStorage.addComment(commentToAdd);
+        return commentStorage.addComment();
+    }
+
+//    @PostMapping("/bens/comments/add")
+//    public String addComment(long songId, String authorName, String commentText) {
+//        Song song = songStorage.retrieveSongById(songId);
+//        Comment commentToAdd = new Comment( long id, song, String authorName   );
+//        commentStorage.addComment(commentToAdd);
+//        return "redirect:/bens/" + ben.getName();
+//    }
+
 }
