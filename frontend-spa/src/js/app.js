de385d5a92d4dd08029ff24ae3ecf2bebb536cd2
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-15 15:02:16 -0400

    continued working on wiring

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -3,3 +3,19 @@ import {
 } from "./pageFetcher";
 
 
+const renderAllArtists = (element, artists) => {
+    clearElementChildren(element);
+
+    for (let i = 0; i < artists.length; i++) {
+        const article = document.createElement('article');
+        article.classList.add('artist');
+        article.innerHTML = `
+            <h2 class="vendor__title">${vendors[i].name}</h2>
+            <h4 class="vendor__phone-number">${vendors[i].phoneNumber}</h4>            
+        `;
+
+        article.addEventListener('click', () => {
+            renderVendor(element, vendors[i]);
+        });
+        element.append(article);
+    };
