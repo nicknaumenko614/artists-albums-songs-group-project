d2afa3caf1abad4203db06777296008d5d067ba9
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-16 22:57:06 -0400

    Added rendering of one artist from a list.

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -1,32 +1,99 @@
 const fetchArtists = async () => {
-  return fetch("http://localhost:8080/api/artists/").then((response) =>
-    response.json()
-  );
+    return fetch("http://localhost:8080/api/artists/").then((response) =>
+        response.json()
+    );
 };
 
-const createArtistsView = (artists) => {
-  const artistsView = document.createElement("main");
-  artistsView.classList.add("artists");
-  artistsView.innerHTML = `
-  <h2>Please select from Artists below:</h2>
-    `;
-  const ul = document.createElement("ul");
-  artists.forEach((artist) => {
-    const li = document.createElement("li");
+const renderAllArtists = (element, artists) => {
+    clearElementChildren(element);
 
-    li.innerHTML += artist.artistName;
-    ul.append(li);
-  });
-  artistsView.append(ul);
+    element.innerHTML = `
+           <h2>Please select from Artists below:</h2>
+        `;
+    for (let i = 0; i < artists.length; i++) {
+        const musiclib = document.createElement('musiclib');
+        musiclib.classList.add('artist');
+        musiclib.innerHTML = `
+            <h4 class="artist__name">${artists[i].artistName}</h4>          
+        `;
 
-  return artistsView;
-};
+        musiclib.addEventListener('click', () => {
+            renderArtist(element, artists[i]);
+        });
+        element.append(musiclib);
+    }
+    ;
+}
 
-fetchArtists().then((artists) => renderArtists(artists));
+const renderArtist = (element, artist) => {
+    clearElementChildren(element);
+    element.innerHTML = `
+      <musiclib class="artist">
+        <h2 class="artist__name">${artist.artistName}</h2>
+        <h4 class="artist__imageurl">${artist.imageUrl}</h4>            
+      </musiclib>
+  `
+    const albums = document.createElement('ul');
 
-const renderArtists = (artistsToDisplay) => {
-  
-  const artistsView = document.querySelector(".artists");
-  console.log(artistsToDisplay);
-  artistsView.appendChild(createArtistsView(artistsToDisplay));
-};
+    artist.albums.forEach((album) => {
+        const li = document.createElement('li');
+        li.innerHTML = `<a class = "album__name">${album.albumName}</a>`
+        li.addEventListener('click', () => {
+            alert(`You clicked on ${album.albumName}!`)
+        })
+        albums.append(li);
+    })
+    const backHomeLink = document.createElement('a');
+    backHomeLink.innerText = "Show All Artists"
+    backHomeLink.addEventListener('click', () => {
+        fetchArtists()
+            .then(artists => {
+                renderAllArtists(library, artists)
+            });
+    })
+    element.append(albums);
+    element.append(backHomeLink);
+}
+
+const clearElementChildren = element => {
+    while (element.firstChild) {
+        element.firstChild.remove();
+    }
+}
+
+const library = document.querySelector(".library")
+fetchArtists()
+    .then(artists => {
+        console.log(artists);
+        renderAllArtists(library, artists)
+    })
+
+// const createArtistsView = (artists) => {
+//     const artistsView = document.createElement("main");
+//     artistsView.classList.add("artists");
+//     artistsView.innerHTML = `
+//   <h2>Please select from Artists below:</h2>
+//     `;
+//     const ul = document.createElement("ul");
+//     artists.forEach((artist) => {
+//         const li = document.createElement("li");
+//
+//         li.innerHTML += artist.artistName;
+//         li.addEventListener('click', () => {
+//             renderArtist(element, artist);
+//         });
+//         ul.append(li);
+//     });
+//     artistsView.append(ul);
+//
+//     return artistsView;
+// };
+//
+// fetchArtists().then((artists) => renderArtists(artists));
+//
+// const renderArtists = (artistsToDisplay) => {
+//
+//     const artistsView = document.querySelector(".artists");
+//     console.log(artistsToDisplay);
+//     artistsView.appendChild(createArtistsView(artistsToDisplay));
+// };
