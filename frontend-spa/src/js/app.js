920f657336cfd5a4ad74ddbc37ae1ab89bbc5d93
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-17 11:34:36 -0400

    wiring artists and albums - in progress

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -11,40 +11,53 @@ const renderAllArtists = (element, artists) => {
            <h2>Please select from Artists below:</h2>
         `;
     for (let i = 0; i < artists.length; i++) {
-        const musiclib = document.createElement('musiclib');
-        musiclib.classList.add('artist');
-        musiclib.innerHTML = `
+        const section = document.createElement('section');
+        section.classList.add('artist');
+        section.innerHTML = `
             <h4 class="artist__name">${artists[i].artistName}</h4>          
         `;
 
-        musiclib.addEventListener('click', () => {
+        section.addEventListener('click', () => {
             renderArtist(element, artists[i]);
         });
-        element.append(musiclib);
+        element.append(section);
+      }
+      ;
     }
-    ;
-}
+    
+    // const renderSong = (element, song) => {
+    //     clearElementChildren(element);
+    //     element.innerHTML = `
+    //   <section class="song">
+    //     <h2 class="song__name">${song.songName}</h2>
+    //     <h2 class="song__duration">${song.duration}</h2>
+    //     <h2 class="song__image">${song.imageUrl}</h2>         
+    //   </section>
+    //   `
 
 const renderArtist = (element, artist) => {
     clearElementChildren(element);
     element.innerHTML = `
-      <musiclib class="artist">
+      <section class="artist">
         <h2 class="artist__name">${artist.artistName}</h2>
-        <h4 class="artist__imageurl">${artist.imageUrl}</h4>            
-      </musiclib>
+        <h4 class="artist__imageurl">${artist.imageUrl}</h4>  
+                  
+      </section>
   `
-    const albums = document.createElement('ul');
-
+  const albums = document.createElement('ul');
+  
     artist.albums.forEach((album) => {
         const li = document.createElement('li');
         li.innerHTML = `<a class = "album__name">${album.albumName}</a>`
-        li.addEventListener('click', () => {
-            alert(`You clicked on ${album.albumName}!`)
-        })
+       
         albums.append(li);
+        li.addEventListener('click', ()=>{
+          alert(album.albumName);
+          // renderAlbum();
+        })
     })
     const backHomeLink = document.createElement('a');
-    backHomeLink.innerText = "Show All Artists"
+    backHomeLink.innerText = "Back To All Artists"
     backHomeLink.addEventListener('click', () => {
         fetchArtists()
             .then(artists => {
@@ -55,6 +68,41 @@ const renderArtist = (element, artist) => {
     element.append(backHomeLink);
 }
 
+
+const renderAlbum = (element, album) => {
+  clearElementChildren(element);
+  element.innerHTML = `
+    <section class="album">
+      <h2 class="album__name">${album.albumName}</h2>
+      <h4 class="album__imageurl">${album.imageUrl}</h4>  
+      <h4 class="album__record-label">${album.recordLabel}</h4> 
+                
+    </section>
+`
+const songs = document.createElement('ul');
+
+  albums.songs.forEach((song) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<a class = "song__name">${song.songName}</a>`
+     
+      songs.append(li);
+      li.addEventListener('click', ()=>{
+        alert(song.songName)
+      })
+  })
+  const backHomeLink = document.createElement('a');
+  backHomeLink.innerText = "Back To All Albums"
+  backHomeLink.addEventListener('click', () => {
+      fetchArtists()
+          .then(artists => {
+              renderAllArtists(library, artists)
+          });
+  })
+  element.append(albums);
+  element.append(backHomeLink);
+}
+
+
 const clearElementChildren = element => {
     while (element.firstChild) {
         element.firstChild.remove();
@@ -64,36 +112,5 @@ const clearElementChildren = element => {
 const library = document.querySelector(".library")
 fetchArtists()
     .then(artists => {
-        console.log(artists);
         renderAllArtists(library, artists)
     })
-
-// const createArtistsView = (artists) => {
-//     const artistsView = document.createElement("main");
-//     artistsView.classList.add("artists");
-//     artistsView.innerHTML = `
-//   <h2>Please select from Artists below:</h2>
-//     `;
-//     const ul = document.createElement("ul");
-//     artists.forEach((artist) => {
-//         const li = document.createElement("li");
-//
-//         li.innerHTML += artist.artistName;
-//         li.addEventListener('click', () => {
-//             renderArtist(element, artist);
-//         });
-//         ul.append(li);
-//     });
-//     artistsView.append(ul);
-//
-//     return artistsView;
-// };
-//
-// fetchArtists().then((artists) => renderArtists(artists));
-//
-// const renderArtists = (artistsToDisplay) => {
-//
-//     const artistsView = document.querySelector(".artists");
-//     console.log(artistsToDisplay);
-//     artistsView.appendChild(createArtistsView(artistsToDisplay));
-// };
